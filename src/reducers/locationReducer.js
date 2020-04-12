42cb89da57ce5ca9c48e0daf651e70bd33cd70f2
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-12 14:31:18 -0700

    added 'LOADING_ACTIONS' & 'FETCH_LOCATIONS'

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -1,5 +1,13 @@
 const locationReducer = (state = { locations: [], loading: false }, action) => {
   switch (action.type) {
+    case 'LOADING_LOCATIONS':
+      return {
+        ...state,
+        locations: [...state.locations],
+        loading: true,
+      };
+    case 'FETCH_LOCATIONS':
+      return { ...state, locations: action.payload };
     default:
       return state;
   }
