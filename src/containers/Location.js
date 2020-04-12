221dd424ecf7566caf170318100b1750604dccf2
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-12 14:30:00 -0700

    added mapStateToProps & mapDispatchToProps

--- src/containers/Location.js
+++ src/containers/Location.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import LocationInput from '../components/LocationInput';
+import Locations from '../components/Locations';
+import { connect } from 'react-redux';
+import { fetchLocations } from '../actions/locationActions';
 
 export class Location extends Component {
+  componentDidMount() {
+    this.props.fetchLocations();
+    console.log('Inside ComponenDidMount');
+  }
+
   render() {
     return (
       <div>
         <h1>In Location Container</h1>
         <LocationInput />
+        <Locations locations={this.props.locations} />
       </div>
     );
   }
 }
 
-export default Location;
+const mapStateToProps = (state) => ({
+  locations: state.locations,
+  loading: state.loading,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchLocations: () => dispatch(fetchLocations()),
+  // addLocation: (formData) => dispatch({ type: 'ADD_RESTAURANT'}, formData)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Location);
