857d50cf8cdfc4f7052cb33908ccde98abdec473
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-13 12:25:13 -0700

    showed index on homepage

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import LocationList from './LocationList';
 
 const Locations = (props) => {
   console.log('In components Locations');
   console.log(props.locations);
-  // const { locations} = this.props;
-  // locations.map( loc => )
+
+  // const locationData = locations.map((loc) => (
+  //   <LocationList key={loc.id} location={loc} />
+  // ));
   return (
     <div>
       <h3>Inside Locations (Index)</h3>
+      {/* <ul>{locationData}</ul> */}
+      {props.locations.map((loc) => (
+        <LocationList key={loc.id} location={loc} />
+      ))}
     </div>
   );
 };
