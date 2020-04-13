52977d219e82c78df13cd2ebf31e04159f58c292
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-13 16:47:11 -0700

    fixed problems on backend to make reducer work

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -1,16 +1,20 @@
 const locationReducer = (state = { locations: [] }, action) => {
   // console.log('locationReducer');
   switch (action.type) {
-    case 'LOADING_LOCATIONS':
-      return {
-        ...state,
-        locations: [...state.locations],
-        loading: true,
-      };
+    // case 'LOADING_LOCATIONS':
+    //   return {
+    //     ...state,
+    //     locations: [...state.locations],
+    //     loading: true,
+    //   };
     case 'FETCH_LOCATIONS':
       // debugger;
       console.log(action);
       return { ...state, locations: action.payload };
+
+    case 'ADD_LOCATION':
+      // debugger;
+      return { ...state, locations: [...state.locations, action.payload] };
     default:
       return state;
   }
