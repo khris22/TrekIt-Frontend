33237f64685a346d0bb77a2453ae488b1dda0577
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-13 21:25:08 -0700

    added Router & NavBars

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Location from '../src/containers/Location';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Navbar from './components/navbar_components/Navbar';
+// import Home from './Home';
+import About from './components/navbar_components/About';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <h1>trekIt</h1>
-        <Location />
+        <Navbar />
+        {/* <Route exact path='/' /> */}
+        <Route exact path='/about' component={About} />
+        <Route exact path='/' component={Location} />
+        {/* <Location /> */}
       </div>
     );
   }
---
+++ src/components/navbar_components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        {/* <NavLink to='/'>Home</NavLink> */}
+        <NavLink to='/'>Home</NavLink>
+        {'   '}
+        <NavLink to='/about'>About</NavLink>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
