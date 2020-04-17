aa2d7f08db5ea9ad74d3483b3d8e6aa739a4eed4
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-16 17:28:47 -0700

    changed data type for :id to match url

--- src/components/LocationShow.js
+++ src/components/LocationShow.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import Adventure from '../containers/Adventure';
 
 const LocationShow = (props) => {
-  debugger;
+  // debugger;
   console.log('INSIDE SHOW PAGE');
   console.log(props);
+
+  // let location = props.locations.filter(
+  //   (loc) => loc.id === parseInt(props.match.params.id),
+  // )[0];
+  const location = props.locations.find(
+    ({ id }) => id === parseInt(props.match.params.id),
+  );
+
+  console.log(location);
+
+  // showPage = () => {};
+
   return (
     <div>
       <h1>LOCATION SHOW PAGE</h1>
+      <h5>{location ? location.park : <p>LOADING</p>}</h5>
+      <Adventure location={location} />
     </div>
   );
 };
