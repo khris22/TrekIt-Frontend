c36c9032f466c4e3d65a0c477fd769a416826dc2
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-16 22:26:03 -0700

    rendered list of adventures for each location

--- src/components/AdventureComponent/Adventures.js
+++ src/components/AdventureComponent/Adventures.js
@@ -1,9 +1,21 @@
 import React from 'react';
+// import Adventure from './Adventure';
 
-const Adventures = () => {
+const Adventures = (props) => {
+  //   debugger;
+  console.log('ADVENTURE', props.adventures);
+  //   const adventure = props.adventures;
   return (
     <div>
       <h4>Show list of past adventures here in Adventures.js</h4>
+      {props.adventures &&
+        props.adventures.map((adv) => (
+          <>
+            <h6>{adv.title}</h6>
+            <p> {adv.memo}</p>
+            <p>{adv.image}</p>
+          </>
+        ))}
     </div>
   );
 };
--- src/containers/Adventure.js
+++ src/containers/Adventure.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import Adventures from '../components/AdventureComponent/Adventures';
 import AdventureInput from '../components/AdventureComponent/AdventureInput';
-import { connect } from 'react-redux';
-import { addAdventure, deleteAdventure } from '../actions/adventureActions';
+// import { connect } from 'react-redux';
+// import { addAdventure, deleteAdventure } from '../actions/adventureActions';
 
 export class Adventure extends Component {
   render() {
     return (
       <div>
         <h5>Your Adventures:</h5>
-        <Adventures />
         <AdventureInput
           addAdventure={this.props.addAdventure}
-          locationId={console.log(this.props)}
+          locationId={console.log('adventureInput', this.props.location)}
+        />
+        <Adventures
+          adventures={this.props.location && this.props.location.adventures}
         />
       </div>
     );
   }
 }
 
-export const mapStateToProps = ({ adventures }) => ({ adventures });
+// export const mapStateToProps = ({ adventures }) => ({ adventures });
 
-export default connect(mapStateToProps, { addAdventure, deleteAdventure })(
-  Adventure,
-);
+export default Adventure;
+// export default connect(mapStateToProps, { addAdventure, deleteAdventure })(
+//   Adventure,
+// );
